e48f3fa30fd41f44056cd8f880259a9bf6e23e50
Shuny <shuny@shunyweb.info>
2019-05-28 21:41:39 +0200

    RG2019

--- js/generation.js
+++ js/generation.js
@@ -1,30 +1,57 @@
 ﻿// Bonjour et bienvenue dans ce code ma foi dégueulasse mais faisant le boulot [:ocube]
 
-var proxyService = "https://www.shunyweb.info/proxy.php";
-var authentificationService = "http://hdfauth.francetv.fr/esi/TA?url=";
-var applicationName = "streamlink";
-var swfParameter = "pvswf=http://staticftv-a.akamaihd.net/player/bower_components/player_flash/dist/FranceTVNVPVFlashPlayer.akamai-05bcfb8a5a233afa06de66e8c03d6642.swf";
-var frenchIpBypass = " --http-header X-Forwarded-For=83.243.20.80";
-var courtToStream = {};
+const livesURL = "https://api-sport-events.webservices.francetelevisions.fr/directs"
+const proxyService = "https://www.shunyweb.info/proxy.php";
+const applicationName = "streamlink";
+const streamInfoURLTemplate = "https://player.webservices.francetelevisions.fr/v1/videos/__ID__?country_code=FR&w=453&h=255&version=5.10.7&domain=sport.francetvinfo.fr&device_type=desktop&browser=chrome&browser_version=74&os=windows&os_version=10.0&gmt=%2B1"
+const frenchIpBypass = " --http-header X-Forwarded-For=83.243.20.80";
 
-courtToStream['DirectFTV'] = 'http://ftvensport04-lh.akamaihd.net/z/SPORT_LV16@323136/manifest.f4m';
-courtToStream['PhilippeChatrier'] = 'http://ftvensport01-lh.akamaihd.net/z/SPORT_LV01@323121/manifest.f4m';
-courtToStream['SuzanneLenglen'] = 'http://ftvensport01-lh.akamaihd.net/z/SPORT_LV02@323122/manifest.f4m';
-courtToStream['Court1'] = 'http://ftvensport01-lh.akamaihd.net/z/SPORT_LV03@323123/manifest.f4m';
-courtToStream['Court2'] = 'http://ftvensport01-lh.akamaihd.net/z/SPORT_LV04@323124/manifest.f4m';
-courtToStream['Court3'] = 'http://ftvensport01-lh.akamaihd.net/z/SPORT_LV05@323125/manifest.f4m';
-courtToStream['Court4'] = 'http://ftvensport02-lh.akamaihd.net/z/SPORT_LV08@323128/manifest.f4m';
-courtToStream['Court5'] = 'http://ftvensport02-lh.akamaihd.net/z/SPORT_LV09@323129/manifest.f4m';
-courtToStream['Court6'] = 'http://ftvensport02-lh.akamaihd.net/z/SPORT_LV06@323126/manifest.f4m';
-courtToStream['Court8'] = 'http://ftvensport02-lh.akamaihd.net/z/SPORT_LV10@323130/manifest.f4m';
-courtToStream['Court10'] = 'http://ftvensport03-lh.akamaihd.net/z/SPORT_LV11@323131/manifest.f4m';
-courtToStream['Court14'] = 'http://ftvensport02-lh.akamaihd.net/z/SPORT_LV07@323127/manifest.f4m';
-courtToStream['Court15'] = 'http://ftvensport03-lh.akamaihd.net/z/SPORT_LV12@323132/manifest.f4m';
-courtToStream['Court16'] = 'http://ftvensport03-lh.akamaihd.net/z/SPORT_LV13@323133/manifest.f4m';
-courtToStream['Court17'] = 'http://ftvensport03-lh.akamaihd.net/z/SPORT_LV14@323134/manifest.f4m';
-courtToStream['Court18'] = 'http://ftvensport03-lh.akamaihd.net/z/SPORT_LV15@323135/manifest.f4m';
-courtToStream['ConfPresse1'] = 'http://ftvensport04-lh.akamaihd.net/z/SPORT_LV17@323137/manifest.f4m';
-courtToStream['ConfPresse2'] = 'http://ftvensport04-lh.akamaihd.net/z/SPORT_LV18@323138/manifest.f4m';
+const qualityList = [
+    {
+        "label":"Meilleure qualité (720p)",
+        "value": "best"
+    }, {
+        "label":"540p",
+        "value": "540p"
+    }, {
+        "label":"432p",
+        "value": "432p"
+    }, {
+        "label":"360p",
+        "value": "360p"
+    }, {
+        "label":"Qualité dégueulasse (216p)",
+        "value": "worst"
+    }];
+   
+var qualitySelection = $("#quality");
+qualityList.forEach(function(quality) {
+    var qualityOption = $("<option>");
+    qualityOption.val(quality["value"]);
+    qualityOption.text(quality["label"]);
+    
+    qualitySelection.append(qualityOption);
+});
+
+var streamInfos = []
+
+function refreshStreamList() {
+    
+    streamInfos = $.post(proxyService, {'url': livesURL},
+    function(serviceResponse) {
+        // Any decent developer would make a check there. I'm too lazy.        
+        streamInfos = JSON.parse(serviceResponse)["page"]["lives"].filter(live => !("status" in live));
+        var courtSelection = $("#court");
+        streamInfos.forEach(function(streamInfo) {
+            var streamOption = $("<option>");
+            streamOption.val(streamInfo["sivideo-id"]);
+            streamOption.text(streamInfo["title"] + " (" + streamInfo["canal"] + ")");
+            
+            courtSelection.append(streamOption);
+        });
+    });
+}
+
 
 function generateNewLink()
 {
@@ -33,24 +60,40 @@ function generateNewLink()
 	var nonFrenchIp = $("#nonfrenchip").is(':checked') ? 1 : 0;
 	var chosenCourt = $("#court").val();
 	var chosenQuality = $("#quality").val();
-	var authentificationUrl = authentificationService + encodeURIComponent(courtToStream[chosenCourt]);
-		
-	$.post(proxyService, { 'url': authentificationUrl},
-		function(streamUrl)
-		{
-			hdsStreamUrl = streamUrl.replace("http", "hds");
-			var cmdLine = applicationName + ' "' + hdsStreamUrl + ' ' + swfParameter + '" ' + chosenQuality;
-			if (nonFrenchIp)
-			{
-				cmdLine += frenchIpBypass;
-			}
-			console.log(nonFrenchIp);
-			$("#link").val(cmdLine);
+	var streamInfoURL = streamInfoURLTemplate.replace("__ID__", chosenCourt)
+	
+    if (!chosenCourt)
+    {
+        return;
+    }
+    authenticationURL = null;
+	$.post(proxyService, { 'url': streamInfoURL},
+		function(streamInfo)
+		{   
+            authenticationURL = JSON.parse(streamInfo)["video"]["token"];
+            if (authenticationURL)
+            {
+                $.post(proxyService, { 'url': authenticationURL},
+                    function(authenticatedStreamInfo)
+                    {   
+                        cmdLine = applicationName + " " + JSON.parse(authenticatedStreamInfo)["url"] + " " + chosenQuality;
+                        if (nonFrenchIp)
+                        {
+                            cmdLine += frenchIpBypass;
+                        }
+                        $("#link").val(cmdLine);
+                    }
+                );
+            }
 		}
-	);
+	);   
 }
 
-// Mise en place des listeners
+$(document).ready(function() {
+    refreshStreamList();
+});
+
+// Listeners
 $(".list").change(generateNewLink);
 $("#nonfrenchip").change(generateNewLink);
 $("#link").on("click",
@@ -59,4 +102,3 @@ $("#link").on("click",
 		$(this).select();
 	}
 );
-$("#court").change(); // Pour que l'URL des valeurs par défaut soit générée quand même
